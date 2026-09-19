a2f3d54e442ce2a050855f6c1fe177a6710ce144
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-19 18:13:39 +0530

    Check get post delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,39 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/test/2" , (req , res) => {
+//     res.send("Hello Test 2")
+// })
 
-app.use("/test" , (req , res) => {
+// app.use("/hello" , (req , res) => {
+//     res.send("Hello Hello")
+// })
+//Work as wild card order is important
+// app.use("/" , (req , res) => {
+//     res.send("Dashboard")
+// })
+
+
+
+app.use("/test", (req, res) => {
     res.send("Hello Test")
 })
-app.use("/hello" , (req , res) => {
-    res.send("Hello Hello")
+
+//order matter if at last then get post delete call happen but it at start then handle all HTTP methods calls
+app.use("/user" , (req,res) => {
+    res.send("use method")
 })
-app.use("/" , (req , res) => {
-    res.send("Dashboard")
+app.get("/user" , (req,res) => {
+    res.send("get call")
+})
+app.post("/user" , (req,res) => {
+    res.send("post call")
+})
+app.delete("/user" , (req,res) => {
+    res.send("delete call")
 })
 
-app.listen(7000 , () => {
+
+app.listen(7000, () => {
     console.log("Server started successfully")
 });
