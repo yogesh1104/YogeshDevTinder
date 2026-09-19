992613aa3ed4ae198ca70f7722e74972ca7f8899
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-19 22:49:49 +0530

    Arry 2 routehandle

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user" , (req,res,next) => {
+app.use("/user" , [(req,res,next) => {
     //Route Handler
     console.log("1st Route Handler for route user")
     next();
@@ -11,7 +11,7 @@ app.use("/user" , (req,res,next) => {
     console.log("2nd Route Handler for route user")
     res.send("2nd Route Handler for route user");
     next();
-},(req,res,next) => {
+}],(req,res,next) => {
     console.log("3rd Route Handler for route user")
     // res.send("3rd Route Handler for route user");
     // next();
