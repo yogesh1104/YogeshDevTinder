6d240c56576526a4ffbb930975f80ce2a2c2b912
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-21 12:26:17 +0530

    Error handling uisng wild card

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 
-const {auth , user} = require("./middlewares/auth")
 const app = express();
 
+//define at last so handle all type of errors
+// app.use("/" , (err , req , res , next) => {
+//     if(err){
+//         res.status(500).send("middleware for handling error at top")
+//     }
+// })
 
-app.get("/admin" , auth)
-
-app.get("/user" , user , (req,res) => {
-    res.send("user passed middleware")
+app.get("/user" , (req,res,next) => {
+    throw new Error("asd")
+    res.send("Error handling")
 })
 
-app.get("/admin/getStatus" , (req,res,next) => {
-    res.send("admin Authorized")
-},(req,res,next) => {
-    res.send("admin auth")
+app.get("/admin" , (req,res) => {
+    try {
+        throw new Error("admin error handle here")
+        res.send("admin")
+    } catch (error) {
+        console.log("Error cought")
+        res.send("handle Error using try catch")
+    }
 })
-
+app.use("/" , (err , req , res , next) => {
+    if(err){
+        res.status(500).send("middleware for handling error at bottom")
+    }
+})
 
 app.listen(7000, () => {
     console.log("Server started successfully")
