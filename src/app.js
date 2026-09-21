ec09d7f0e4d2bda75f1e9cc5578183342e0ed71a
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-21 18:05:38 +0530

    API level senitization

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.get("/user", async (req, res) => {
             res.send(user)
         }
     } catch (error) {
-        res.status(500).send("Something went wrong"+ error.message)
+        res.status(500).send("Something went wrong" + error.message)
     }
 })
 
@@ -41,43 +41,54 @@ app.get("/feed", async (req, res) => {
             res.send(users)
         }
     } catch (error) {
-        res.status(500).send("Something went wrong"+ error.message)
+        res.status(500).send("Something went wrong" + error.message)
     }
 })
 
 //Delete user using id and email
-app.delete("/user" , async (req,res) => {
+app.delete("/user", async (req, res) => {
     // const userId = req.body.userId;;
     const emailId = req.body.emailId;
     try {
         // const user = await User.findByIdAndDelete({_id : userId})
         // const user = await User.findByIdAndDelete(userId)
-        const user = await User.findOneAndDelete({emailId : emailId})
+        const user = await User.findOneAndDelete({ emailId: emailId })
         res.send("user deleted successfully")
-        console.log("deleted User => " , user)
+        console.log("deleted User => ", user)
     } catch (error) {
-        res.status(500).send("Something went wrong"+ error.message)
+        res.status(500).send("Something went wrong" + error.message)
     }
 })
 
 //update the user using id
-app.patch("/user" , async (req,res) => {
+app.patch("/user/:userId", async (req, res) => {
     // const userId = req.body.userId
-    const inputEmail = req.body.inputEmailId
+    // const inputEmail = req.body.inputEmailId
+    const userId = req.params?.userId
     const data = req.body
     //userId will not added in database bcoz userId is not in user schema
     try {
-        // const user = await User.findOneAndUpdate({_id : userId} , data , {
-        //     returnDocument : 'before' // you can use after also
+
+        const ALLOWED_UPDATES = ["age", "gender", "skills", "photoUrl"];
+        const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k))
+        if (!isUpdateAllowed) {
+            throw new Error("Update not Allowed")
+        }
+        if(req.body.skills.length > 10){
+            throw new Error("Skill not more then 10")
+        }
+        // const user = await User.findOneAndUpdate({emailId : inputEmail} , data , {
+        //     returnDocument : 'before',
+        //     runValidators : true
         // })
-        const user = await User.findOneAndUpdate({emailId : inputEmail} , data , {
-            returnDocument : 'before',
-            runValidators : true
+        const user = await User.findOneAndUpdate({ _id: userId }, data, {
+            returnDocument: 'before', // you can use after also
+            runValidators: true
         })
         res.send("user updated successfully")
-        console.log("Before updated data => " , user)
+        console.log("Before updated data => ", user)
     } catch (error) {
-        res.status(500).send("Something went wrong"+ error.message)
+        res.status(500).send("Something went wrong " + error.message)
     }
 })
 
