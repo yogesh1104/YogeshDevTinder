5973a995037b8b23b945e1bcfd2ce9cfd5eb5b93
yogesh1104 <yogeshgupta8982@gmail.com>
2026-09-21 16:04:06 +0530

    API for delete and update

--- src/app.js
+++ src/app.js
@@ -17,6 +17,69 @@ app.post("/signup", async (req, res) => {
 
 })
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId
+    try {
+        const user = await User.find({ emailId: userEmail })
+        if (user.length == 0) {
+            res.status(404).send("user not found")
+        } else {
+            res.send(user)
+        }
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+})
+
+//feed api to get all the users
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({})
+        if (users.length == 0) {
+            res.status(404).send("user not found")
+        } else {
+            res.send(users)
+        }
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+})
+
+//Delete user using id and email
+app.delete("/user" , async (req,res) => {
+    // const userId = req.body.userId;;
+    const emailId = req.body.emailId;
+    try {
+        // const user = await User.findByIdAndDelete({_id : userId})
+        // const user = await User.findByIdAndDelete(userId)
+        const user = await User.findOneAndDelete({emailId : emailId})
+        res.send("user deleted successfully")
+        console.log("deleted User => " , user)
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+})
+
+//update the user using id
+app.patch("/user" , async (req,res) => {
+    // const userId = req.body.userId
+    const inputEmail = req.body.inputEmailId
+    const data = req.body
+    //userId will not added in database bcoz userId is not in user schema
+    try {
+        // const user = await User.findOneAndUpdate({_id : userId} , data , {
+        //     returnDocument : 'before' // you can use after also
+        // })
+        const user = await User.findOneAndUpdate({emailId : inputEmail} , data , {
+            returnDocument : 'before' 
+        })
+        res.send("user updated successfully")
+        console.log("Before updated data => " , user)
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+})
+
 connectDb().then(() => {
     console.log("Database connection established!")
     app.listen(7000, () => {
